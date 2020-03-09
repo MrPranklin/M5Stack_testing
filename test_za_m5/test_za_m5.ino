4a217d66016d2fdc08ca109031a9f249892407c8
Filip Pranklin <filip.pranklin@gmail.com>
2020-03-09 09:31:44 +0100

    enable OTA

--- test_za_m5/test_za_m5.ino
+++ test_za_m5/test_za_m5.ino
@@ -2,7 +2,109 @@
 #include "DHT22_C.h"
 #include "m5lcd.h"
 
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <Update.h>
+
 #define DHTPIN 26
+
+const char* host = "M5Stack";
+const char* ssid = "Martin Ruter King";
+const char* password = "volimkonje";
+
+WebServer server(80);
+
+/*
+   Login page
+*/
+
+const char* loginIndex =
+  "<form name='loginForm'>"
+  "<table width='20%' bgcolor='A09F9F' align='center'>"
+  "<tr>"
+  "<td colspan=2>"
+  "<center><font size=4><b>ESP32 Login Page</b></font></center>"
+  "<br>"
+  "</td>"
+  "<br>"
+  "<br>"
+  "</tr>"
+  "<td>Username:</td>"
+  "<td><input type='text' size=25 name='userid'><br></td>"
+  "</tr>"
+  "<br>"
+  "<br>"
+  "<tr>"
+  "<td>Password:</td>"
+  "<td><input type='Password' size=25 name='pwd'><br></td>"
+  "<br>"
+  "<br>"
+  "</tr>"
+  "<tr>"
+  "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
+  "</tr>"
+  "</table>"
+  "</form>"
+  "<script>"
+  "function check(form)"
+  "{"
+  "if(form.userid.value=='admin' && form.pwd.value=='admin')"
+  "{"
+  "window.open('/serverIndex')"
+  "}"
+  "else"
+  "{"
+  " alert('Error Password or Username')/*displays error message*/"
+  "}"
+  "}"
+  "</script>";
+
+/*
+   Server Index Page
+*/
+
+const char* serverIndex =
+  "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+  "<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+  "<input type='file' name='update'>"
+  "<input type='submit' value='Update'>"
+  "</form>"
+  "<div id='prg'>progress: 0%</div>"
+  "<script>"
+  "$('form').submit(function(e){"
+  "e.preventDefault();"
+  "var form = $('#upload_form')[0];"
+  "var data = new FormData(form);"
+  " $.ajax({"
+  "url: '/update',"
+  "type: 'POST',"
+  "data: data,"
+  "contentType: false,"
+  "processData:false,"
+  "xhr: function() {"
+  "var xhr = new window.XMLHttpRequest();"
+  "xhr.upload.addEventListener('progress', function(evt) {"
+  "if (evt.lengthComputable) {"
+  "var per = evt.loaded / evt.total;"
+  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+  "}"
+  "}, false);"
+  "return xhr;"
+  "},"
+  "success:function(d, s) {"
+  "console.log('success!')"
+  "},"
+  "error: function (a, b, c) {"
+  "}"
+  "});"
+  "});"
+  "</script>";
+
+/*
+   setup function
+*/
 
 state_n::StateEnum state = state_n::temperature;
 
@@ -12,69 +114,133 @@ float temp = 0.0;
 float hum = 0.0;
 
 void setup() {
-    M5.begin(true, false, false, false);
-    m5lcd::begin();
+  Serial.begin(115200);
 
-    Serial.begin(9600);
-    Serial.println("Started");
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);
+  WiFi.setHostname(host);
+  Serial.println("");
 
-    dht22.begin();
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
-    hum = dht22.read_humidity();
-    temp = dht22.read_temperature();
+  /*use mdns for host name resolution*/
+  if (!MDNS.begin(host)) {  //http://esp32.local
+    Serial.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+  /*return index page which is stored in serverIndex */
+  server.on("/", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", loginIndex);
+  });
+  server.on("/serverIndex", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", serverIndex);
+  });
+  /*handling uploading firmware file */
+  server.on("/update", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      Serial.printf("Update: %s\n", upload.filename.c_str());
+      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      /* flashing firmware to ESP*/
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) { //true to set the size to the current progress
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      } else {
+        Update.printError(Serial);
+      }
+    }
+  });
+  server.begin();
 
-    m5lcd::update_display(state, temp, hum);
+  M5.begin(true, false, false, false);
+  m5lcd::begin();
+
+  Serial.println("Started");
+
+  dht22.begin();
+
+  hum = dht22.read_humidity();
+  temp = dht22.read_temperature();
+
+  m5lcd::update_display(state, temp, hum);
 }
 
 void loop() {
-    check_buttons();
+  server.handleClient();
+  delay(1);
 
-    if (m5lcd::is_display_on() && dht22.is_sensor_ready(2000)) {
-        update_values(state);
-    }
+  check_buttons();
+
+  if (m5lcd::is_display_on() && dht22.is_sensor_ready(2000)) {
+    update_values(state);
+  }
 }
 
 void update_values(state_n::StateEnum state) {
-    switch (state) {
-        case state_n::temperature: {
-            float new_temp = dht22.read_temperature();
-            if (temp != new_temp) {
-                temp = new_temp;
-                m5lcd::update_display(state, temp, hum);
-            }
-            break;
+  switch (state) {
+    case state_n::temperature: {
+        float new_temp = dht22.read_temperature();
+        if (temp != new_temp) {
+          temp = new_temp;
+          m5lcd::update_display(state, temp, hum);
         }
-        case state_n::humidity: {
-            float new_hum = dht22.read_humidity();
-            if (hum != new_hum) {
-                hum = new_hum;
-                m5lcd::update_display(state, temp, hum);
-            }
-            break;
+        break;
+      }
+    case state_n::humidity: {
+        float new_hum = dht22.read_humidity();
+        if (hum != new_hum) {
+          hum = new_hum;
+          m5lcd::update_display(state, temp, hum);
         }
-    }
+        break;
+      }
+  }
 }
 
 void set_state(state_n::StateEnum new_state) {
-    state = new_state;
-    Serial.print("State set to: ");
-    Serial.println(state);
-    m5lcd::update_display(state, temp, hum);
-    return;
+  state = new_state;
+  Serial.print("State set to: ");
+  Serial.println(state);
+  m5lcd::update_display(state, temp, hum);
+  return;
 }
 
 void check_buttons() {
-    M5.update();
+  M5.update();
 
-    if (M5.BtnA.wasPressed()) {
-        m5lcd::set_display_state(true);
-        set_state(state_n::temperature);
-    } else if (M5.BtnB.wasPressed()) {
-        m5lcd::set_display_state(true);
-        set_state(state_n::humidity);
-    } else if (M5.BtnC.wasPressed()) {
-        m5lcd::toggle_display();
-    }
+  if (M5.BtnA.wasPressed()) {
+    m5lcd::set_display_state(true);
+    set_state(state_n::temperature);
+  } else if (M5.BtnB.wasPressed()) {
+    m5lcd::set_display_state(true);
+    set_state(state_n::humidity);
+  } else if (M5.BtnC.wasPressed()) {
+    m5lcd::toggle_display();
+  }
 
-    return;
+  return;
 }
