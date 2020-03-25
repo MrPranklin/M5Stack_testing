99791ebd2bc5a87111ce659add263ffa47da5218
Filip Pranklin <filip.pranklin@gmail.com>
2020-03-25 21:35:46 +0100

    uncomment things

--- test_za_m5/MockHeater.cpp
+++ test_za_m5/MockHeater.cpp
@@ -1,19 +1,19 @@
 #include "MockHeater.hpp"
-//#include <Arduino.h>
+#include <Arduino.h>
 
 MockHeater::MockHeater(int pin) : Heater() {
     this->_pin = pin;
-//    pinMode(this->_pin, OUTPUT);
+    pinMode(this->_pin, OUTPUT);
 }
 
 void MockHeater::turnOn() {
     this->_isEnabled = true;
-//    digitalWrite(this->_pin, HIGH);
+    digitalWrite(this->_pin, HIGH);
 }
 
 void MockHeater::turnOff() {
     this->_isEnabled = false;
-//    digitalWrite(this->_pin, LOW);
+    digitalWrite(this->_pin, LOW);
 }
 
 bool MockHeater::isEnabled(){
