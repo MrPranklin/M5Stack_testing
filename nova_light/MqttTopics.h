f06abf4689500a71b15c2d6fecca2682476fc0e3
Filip Pranklin <filip.pranklin@gmail.com>
2020-04-25 15:22:14 +0200

    update topics

--- nova_light/MqttTopics.h
+++ nova_light/MqttTopics.h
@@ -1,7 +1,7 @@
 #ifndef MQTTTOPICS_H
 #define MQTTTOPICS_H
 
-#define mqtt_command_blinds "NOVA/command/blinds"
-#define mqtt_command_light "NOVA/command/light"
+#define mqtt_command_blinds "NOVA/command/naturalLight"
+#define mqtt_command_light "NOVA/command/artificialLight"
 
 #endif // MQTTTOPICS_H
